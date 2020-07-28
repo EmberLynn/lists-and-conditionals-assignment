93f13f62a9c71a107a412d276a7a8a65af0aae56
EmberLynn <ember.robb@gmail.com>
2020-07-28 09:45:06 -0600

    Added CharOutput component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation';
+import CharOutput from './CharOutput/CharOutput';
 
 class App extends Component {
   state = {
+    inputString: null,
     stringLength: 0
   }
 
   textChangeListener = (event) => {
-    const stringLength = event.target.value.length;
-    this.setState({stringLength: stringLength})
+    const inputString = event.target.value;
+    const stringLength = inputString.length;
+    this.setState(
+      {
+        stringLength: stringLength,
+        inputString: inputString
+      }
+      )
   }
 
   render(){
+
+    // let chars = null;
+    // let charsArray = this.state.inputString.split;
+    // chars = (
+    //     <div>
+    //       {charsArray.map((char) => {
+    //         return <CharOutput/>
+    //     })}
+    //     </div>
+    // );
+
+
     return (
       //create input with change listener that outputs the length of entered text
       <div className='App'>
---
+++ src/CharOutput/CharOutput.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Char from './CharOutput.css'
+
+const charOutput = (props) => {
+    return (
+        <div className='Char'></div>
+    )
+}
+
+export default charOutput;
