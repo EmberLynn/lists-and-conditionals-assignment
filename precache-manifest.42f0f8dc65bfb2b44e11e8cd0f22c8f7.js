ca22b17c2cf8b2362b33999cc3bae067b57baed2
EmberLynn <ember.robb@gmail.com>
2020-07-29 09:55:32 -0600

    Updates

---
+++ precache-manifest.42f0f8dc65bfb2b44e11e8cd0f22c8f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7d6dcebe5e11c71f8b7c0582f2c4d7",
+    "url": "/lists-and-conditionals-assignment/index.html"
+  },
+  {
+    "revision": "83fc6b134f945452abaf",
+    "url": "/lists-and-conditionals-assignment/static/css/main.682a09c1.chunk.css"
+  },
+  {
+    "revision": "86b97acf0b2b72359b61",
+    "url": "/lists-and-conditionals-assignment/static/js/2.271d5ae5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lists-and-conditionals-assignment/static/js/2.271d5ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83fc6b134f945452abaf",
+    "url": "/lists-and-conditionals-assignment/static/js/main.410bccb5.chunk.js"
+  },
+  {
+    "revision": "6133487cc4aea9415952",
+    "url": "/lists-and-conditionals-assignment/static/js/runtime-main.3b29b647.js"
+  }
+]);
